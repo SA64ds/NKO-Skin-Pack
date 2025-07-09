035dac5bb4d609aef1e185e9bceb7a442b6949a1
SA64ds <sa64ds@outlook.com>
2025-07-09 00:52:00 -0400

    First commit

---
+++ characters/nate.lua
@@ -0,0 +1,21 @@
+function onCreate()
+    -- Checks if you are playing as Nate or not connected to a room before switching everything.
+    if playsAsBF()==false or isRoomConnected()==false then
+    setPropertyFromClass('substates.GameOverSubstate', 'deathSoundName', 'fnf_loss_sfx'); --Death sound effect
+    setPropertyFromClass('substates.GameOverSubstate', 'loopSoundName', 'gameOver-nate'); --Game Over Loop Music
+    setPropertyFromClass('substates.GameOverSubstate', 'endSoundName', 'gameOverEND-nate'); --Game Over End Music
+
+    setPropertyFromClass("substates.PauseSubState", "songName", "breakfast-nate"); --Pause Music
+
+    -- Sticker Pack was removed since Psych Online doesn't use the sticker transition.
+
+    end
+
+end
+
+function onUpdate()
+    --if inGameOver == true then
+        --setProperty('camFollow.x', getGraphicMidpointX('boyfriend'))
+        --setProperty('camFollow.y', getGraphicMidpointY('boyfriend'))
+    --end
+end
